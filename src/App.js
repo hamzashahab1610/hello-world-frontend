07db8bdd116d41c89d65e5734bec0475222c8095
hamzashahab1610 <hamzashahab1610@gmail.com>
2020-09-20 20:50:20 +0500

    initializing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+	return (
+		<div className="App">
+			<header className="App-header">
+				{/* <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -17,10 +17,11 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
-      </header>
-    </div>
-  );
+        </a> */}
+				<h1>Hello World Frontend</h1>
+			</header>
+		</div>
+	);
 }
 
 export default App;
